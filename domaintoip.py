b1c2f2a0eae838e5ab91a6ad8b6663b3e56efd85
Rhmansh <60416516+Rhmansh@users.noreply.github.com>
2022-12-13 17:19:17 +0700

    update

--- domaintoip.py
+++ domaintoip.py
@@ -1,36 +1,33 @@
-import os
-import socket
-from colorama import Fore, Back, Style
-
-path = "domains.txt"
-exs = os.path.exists(path)
-print(Fore.CYAN+"# Name: Simple Mass Domain To IP")
-print("# Author: Rahman Ralei")
-print("# Script: Python3")
-print("# Version: V1")
-ah = input("\nInput (y) untuk run program: "+Style.RESET_ALL).lower()
-grs = "-------------------------------------------------------------------------------"
-if(ah == "y"):
-    if exs == True:
-    	res = []
-    	print(grs)
-    	print(Fore.GREEN+"[!] Loading, Please Wait...\n"+Style.RESET_ALL)
-    	with open("domains.txt") as f:
-    		daftar_domain = f.read().splitlines()
-    		for domain in daftar_domain:
-    		   	try:
-    		   		ip = socket.gethostbyname(domain)
-    		   		print(Fore.YELLOW+f"{domain}"+Style.RESET_ALL+" -> "+Fore.GREEN+f"{ip}"+Style.RESET_ALL)
-    		   		res.append(ip)  		
-    		   	except socket.gaierror:
-    		   		print(Fore.YELLOW+f"{domain}"+Style.RESET_ALL+" -> "+Fore.RED+"Not Found!"+Style.RESET_ALL)
-    	with open("result-ip.txt", "w") as sv:
-    		cnt = ""
-    		for cnt, list in enumerate(res):
-    			sv.write(str(list)+"\n")
-    		print(Fore.CYAN+f"\n[!] Done!!!\n- Result IP : {cnt + 1}\n- Ke simpen di file (result-ip.txt)"+Style.RESET_ALL)
-    		print(grs)
-    if exs == False:
-    	print(f"\nFile {path} gaada di directory lu! \nSilahkan buat filenya dulu di dalem directory yang sama.")
-else:
-	print('\nUpss Program telah terhenti!\nJika ingin menggunakan kembali silahkan buka kembali scriptnya.')
+S=' -> '
+R='domains.txt'
+Q=open
+A=print
+import os,socket as E
+from colorama import Fore as B,Back,Style as C
+def L(strings):
+	A=[]
+	for B in strings:
+		if B not in A:A.append(B)
+	return A
+F=R
+G=os.path.exists(F)
+A(B.CYAN+'# Name: Simple Mass Domain To IP')
+A('# Author: Rahman Ralei')
+A('# Script: Python3')
+A('# Version: V1')
+M=input('\nInput (y) untuk run program: '+C.RESET_ALL).lower()
+H='-------------------------------------------------------------------------------'
+if M=='y':
+	if G==True:
+		I=[];A(H);A(B.GREEN+'[!] Loading, Please Wait...\n'+C.RESET_ALL)
+		with Q(R)as N:
+			O=N.read().splitlines()
+			for D in L(O):
+				try:J=E.gethostbyname(D);A(B.YELLOW+f"{D}"+C.RESET_ALL+S+B.GREEN+f"{J}"+C.RESET_ALL);I.append(J)
+				except E.gaierror:A(B.YELLOW+f"{D}"+C.RESET_ALL+S+B.RED+'Not Found!'+C.RESET_ALL)
+		with Q('result-ip.txt','w')as P:
+			K=''
+			for (K,list) in enumerate(I):P.write(str(list)+'\n')
+			A(B.CYAN+f"\n[!] Done!!!\n- Result IP : {K+1}\n- Ke simpen di file (result-ip.txt)"+C.RESET_ALL);A(H)
+	if G==False:A(f"\nFile {F} gaada di directory lu! \nSilahkan buat filenya dulu di dalem directory yang sama.")
+else:A('\nUpss Program telah terhenti!\nJika ingin menggunakan kembali silahkan buka kembali scriptnya.')
