35c4daed49f30327d0178eac65bc81058fd11198
Rhmansh <60416516+Rhmansh@users.noreply.github.com>
2022-12-13 19:38:57 +0700

    update
    update

--- domaintoip.py
+++ domaintoip.py
@@ -1,33 +1,36 @@
-S=' -> '
-R='domains.txt'
-Q=open
+R=' => '
+Q='domains.txt'
+P=open
 A=print
-import os,socket as E
+import os,socket as G
 from colorama import Fore as B,Back,Style as C
-def L(strings):
+def H(strings):
 	A=[]
 	for B in strings:
 		if B not in A:A.append(B)
 	return A
-F=R
-G=os.path.exists(F)
+I=Q
+J=os.path.exists(I)
 A(B.CYAN+'# Name: Simple Mass Domain To IP')
 A('# Author: Rahman Ralei')
 A('# Script: Python3')
 A('# Version: V1')
-M=input('\nInput (y) untuk run program: '+C.RESET_ALL).lower()
-H='-------------------------------------------------------------------------------'
-if M=='y':
-	if G==True:
-		I=[];A(H);A(B.GREEN+'[!] Loading, Please Wait...\n'+C.RESET_ALL)
-		with Q(R)as N:
-			O=N.read().splitlines()
-			for D in L(O):
-				try:J=E.gethostbyname(D);A(B.YELLOW+f"{D}"+C.RESET_ALL+S+B.GREEN+f"{J}"+C.RESET_ALL);I.append(J)
-				except E.gaierror:A(B.YELLOW+f"{D}"+C.RESET_ALL+S+B.RED+'Not Found!'+C.RESET_ALL)
-		with Q('result-ip.txt','w')as P:
-			K=''
-			for (K,list) in enumerate(I):P.write(str(list)+'\n')
-			A(B.CYAN+f"\n[!] Done!!!\n- Result IP : {K+1}\n- Ke simpen di file (result-ip.txt)"+C.RESET_ALL);A(H)
-	if G==False:A(f"\nFile {F} gaada di directory lu! \nSilahkan buat filenya dulu di dalem directory yang sama.")
+L=input('\n# Input (y) untuk run program: '+C.RESET_ALL).lower()
+K='-------------------------------------------------------------------------------'
+if L=='y':
+	if J==True:
+		D=[];A(K);A(B.GREEN+'[!] Loading, Please Wait...\n'+C.RESET_ALL)
+		with P(Q)as M:
+			N=M.read().splitlines()
+			for F in H(N):
+				try:E=G.gethostbyname(F);A(B.YELLOW+f"{F}"+C.RESET_ALL+R+B.GREEN+f"{E}"+C.RESET_ALL);D.append(E)
+				except G.gaierror:A(B.YELLOW+f"{F}"+C.RESET_ALL+R+Back.RED+'Not Found!'+C.RESET_ALL)
+		with P('result-ip.txt','w')as O:
+			D=list(H(D))
+			for (E,list) in enumerate(D):O.write(str(list)+'\n')
+			A(B.CYAN+f"""
+# Done!
+# Total IP + Remove Duplicate : {E+1}
+# Saved (result-ip.txt)"""+C.RESET_ALL);A(K)
+	if J==False:A(f"\nFile {I} gaada di directory lu! \nSilahkan buat filenya dulu di dalem directory yang sama.")
 else:A('\nUpss Program telah terhenti!\nJika ingin menggunakan kembali silahkan buka kembali scriptnya.')
